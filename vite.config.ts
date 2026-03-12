978f0dd3ee98317819249b55800aa86cf4ee7e00
naimniloy1 <naimniloy1@gmail.com>
2026-03-13 01:46:08 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,10 @@
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./client/src"),
-      "@shared": path.resolve(__dirname, "./shared"),
-    },
-  },
-  root: path.resolve(__dirname, "client"),
-  build: {
-    outDir: path.resolve(__dirname, "dist/public"),
-    emptyOutDir: true,
+  server: {
+    port: 3000,
   },
 });
